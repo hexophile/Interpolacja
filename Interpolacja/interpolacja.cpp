45b3972b95815ebad039046a31bbcc3216e3bd11
jak <jaksen95@gmail.com>
2016-12-31 14:52:11 +0100

    Usunięcie niepotrzebnego parametru z oblicz_L()

--- Interpolacja/interpolacja.cpp
+++ Interpolacja/interpolacja.cpp
@@ -192,9 +192,9 @@ float *wypelnij_fp(int &np, float *xp)
 }
 
 // Funkcja obliczaj¹ca wartoœæ L(x) w danym punkcie
-float oblicz_L(int &n, int &np, float *x, float &xp, float *f)
+float oblicz_L(int &n, float *x, float &xp, float *f)
 {
-	if ((n > 0) && (np > 0) && (x != NULL)) {
+	if ((n > 0) && (x != NULL)) {
 		float temp = 0;
 		float temp1, temp2;
 
@@ -210,9 +210,6 @@ float oblicz_L(int &n, int &np, float *x, float &xp, float *f)
 		string exc;
 		if (n <= 0) {
 			exc = "InsufficientNodesAmountException: n <= 0; at ";
-		}
-		else if (np <= 0) {
-			exc = "InsufficientNodesAmountException: np <= 0; at ";
 		}
 		else if (x == NULL) {
 			exc = "NullPointerException: parameter float *x; at ";
@@ -241,7 +238,7 @@ float *wypelnij_L(int &n, int &np, float *x, float *xp, float *f)
                 temp +=  temp1 * temp2;        //a i omega s¹ tego samego stopnia
             }
 */
-            L[i] = oblicz_L(n, np, x, xp[i], f);
+            L[i] = oblicz_L(n, x, xp[i], f);
 			temp = 0;
         }
         return L;
